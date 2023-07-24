38fbff1e03642606d9969f77aeeea23017e2732a
KEINance <keinance2@gmail.com>
2023-07-23 21:37:06 -0400

    addemployee changes

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,10 @@ CREATE TABLE employees(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     manager_id INT,
-    role_id INT 
+    role_id INT,
+    job_title VARCHAR(50),
+    departments VARCHAR(50),
+    salaries VARCHAR(30)
 );
 
 INSERT INTO department(name)
@@ -31,5 +34,5 @@ VALUES('engineer', 80000, 1), ('manager', 70000, 2), ('legal', 120000, 3);
 
 
 INSERT INTO employees(first_name, last_name, manager_id, role_id, job_title, departments, salaries)
-VALUES('John', 'Doe', NULL, 2, 'CEO', 'ALL', 'Too Much'),('Jane', 'Doe', 1, 1, 'engineer'. 80000), ('Millie', 'Jonhns', 2, 3, 'management', 70000);
+VALUES('John', 'Doe', 1, 2, 1, 'management', 200000),('Jane', 'Doe', 1, 1, 'engeneering', 'manager', 80000), ('Millie', 'Jonhns', 2, 3, 'management', 'legal', 70000);
 
--- server.js
+++ server.js
@@ -237,7 +237,7 @@ function addEmployee(employees) {
       })
     })
   })
-};
+}
 
 
 
